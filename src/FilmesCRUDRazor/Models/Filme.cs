148c49c57a0de57879d31b26dcf8c2725796d288
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-10-14 00:15:46 -0300

    Inclusão de DataAnnotations na classe 'Filme'

--- src/FilmesCRUDRazor/Models/Filme.cs
+++ src/FilmesCRUDRazor/Models/Filme.cs
@@ -1,4 +1,6 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FilmesCRUDRazor.Models 
 {
@@ -6,12 +8,21 @@ namespace FilmesCRUDRazor.Models
     {
         public int FilmeId { get; set; }
 
+        [Display(Name = "Título")]
+        [Required]
         public string Titulo { get; set; }
 
+        [Display(Name = "Data de Lançamento")]
+        [DisplayFormat(DataFormatString = "{0: dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime DataLancamento { get; set; }
 
+        [Display(Name="Gênero")]
+        [Required]
         public string Genero { get; set; }
 
+        [Display(Name = "Preço")]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Preco { get; set; }
     }
 }
